59dddaeb2a6ca7f5bc41c3f35768cab9f84126ce
Yannick6633 <yanlechaka@sfr.fr>
2019-05-15 09:33:46 +0200

    récupérer plusieurs objets avec des filtres

--- README.md
+++ README.md
@@ -39,4 +39,29 @@ Critères de validation n°5 : Créer ta première entité avec Doctrine.
     Ton code devra être disponible sur un repository GitHub.
 
 
+Critères de validation n°6 : Crée la relation ManyToOne.
+
+    La propriété category est présente et privée.
+    Le getter et setter correspondants sont présents et publics.
+    Les annotations ManyToOne et JoinColumn sont présentes et correctement paramétrées (nullable=false, targetEntity)
+    Le use nécessaire pour les annotations @ORM est en place.
+    Ton code devra être disponible sur un repository GitHub.
+    
+    
+Critères de validation n°7 : Récupérer des données stockées avec Doctrine.
+    
+    FindBy() : récupérer plusieurs objets avec des filtres
+    
+        Une nouvelle méthode showByCategory(string $categoryName) a été créée dans le controller BlogController.
+        La route de cette méthode sera sous la forme : @Route("/category/{category}", name="show_category").
+        Cette méthode retourne un tableau d'articles récupéré par une méthode de type findBy(), en limitant le nombre de résultats à 3, le tout trié par id décroissant.
+        Un nouveau fichier a été créé templates/blog/category.html.twig.
+        Ce fichier bouclera sur tous les articles afin d'afficher l'id, le titre et le contenu de chaque itération.
+        L'URL http://localhost:8000/blog/category/javascript est fonctionnelle et renvoie bien tous les articles liés à la catégorie Javascript, ajoutée en début de quête.
+        Ton code devra être disponible sur un repository GitHub
+    
+
+
+
+
 
--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -3,30 +3,108 @@
 
 namespace App\Controller;
 
+
+
+use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/blog")
+ */
 class BlogController extends AbstractController
 {
 
     /**
-     * @Route("/blog", name="blog_index")
+     * Show all row from article's entity
+     *
+     * @Route("/", name="blog_index")
+     * @return Response A response instance
      */
 
-    public function index()
+    public function index(): Response
     {
-        return $this->render('blog/index.html.twig', ['owner' => 'Yannick']);
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render('blog/index.html.twig',
+            ['articles' => $articles]
+        );
     }
 
     /**
-    * @Route("/blog/show/{slug<[a-z-0-9]+>}", methods={"GET"}, name="blog_show")
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug the slugger
+     *
+     * @Route("/{slug<^[a-z0-9-]+$>}",
+     *      defaults={"slug" = null},
+     *      name="blog_show")
+     * @return Response A response instance
      */
 
-    public function show($slug = "Article Sans Titre")
+    public function show(?string $slug) : Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with ' . $slug . ' title, found in article\'s table.'
+            );
+
+        }
+
+        return $this->render(
+        'blog/show.html.twig',
+        [
+            '$article' => $article,
+            'slug' => $slug
+        ]
+    );
+
+    }
+
+    /**
+     * @Route("/category/{categoryName}", name="show_category")
+     */
+
+    public function ShowByCategory(string $categoryName)
     {
 
-        $slug = ucwords(str_replace("-"," ", $slug));
-        return $this->render('blog/show.html.twig', ['slug' => $slug]);
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => $categoryName]);
+
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy(['category' => $category], ['id' => 'DESC'], 3 );
+
+        return $this->render('blog/category.html.twig', ['articles' => $articles]);
+
     }
+
 }
+
+
+
